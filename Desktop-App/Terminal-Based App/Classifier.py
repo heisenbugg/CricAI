e0d4069ba99be8859155f4fce8faea3e8ee381dc
heisenbugg <pritish.rulz096@gmail.com>
2017-12-11 16:59:52 +0530

    Update Classifier.py

--- Desktop-App/Terminal-Based App/Classifier.py
+++ Desktop-App/Terminal-Based App/Classifier.py
@@ -4,6 +4,7 @@ import pickle
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 from sklearn import tree
+from sklearn import svm
 
 d = datasetRead.Dataset()
 dIF = dataInputFormat.DataInput()
